ebed5c8d98f533ed45ae97a66a0ba6ecc525cd0c
conmui <175262420+conmui@users.noreply.github.com>
2025-03-15 21:02:30 -0400

    API 5
    API 5: POST To Search Product
    API URL: https://automationexercise.com/api/searchProduct
    Request Method: POST
    Request Parameter: search_product (For example: top, tshirt, jean)
    Response Code: 200
    Response JSON: Searched products list

---
+++ src/test/java/APITest5.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.Map;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+public class APITest5 extends BaseTest {
+    @Test
+    public void postSearchProduct_checkProductsListRelatedToSearch() {
+        BaseService baseService = new BaseService();
+        String queryValue = "jean";
+        Response postResponse = baseService.postRequestWithQuery( "/searchProduct", "search_product", queryValue);
+        List<Map<String, Object>> productsList = baseService.getResponseList(postResponse,"products");
+
+        assertThat(postResponse.getStatusCode(), equalTo(OK_RESPONSE_STATUS_CODE));
+
+        assertThat(baseService.getResponseCode(postResponse), equalTo(OK_RESPONSE_STATUS_CODE));
+
+        verifyProductsRelatedToSearch(productsList, queryValue);
+    }
+
+    public void verifyProductsRelatedToSearch(List<Map<String, Object>> productsList, String searchText) {
+        for (Map<String, Object> product : productsList) {
+            String productName = product.get("name").toString().toLowerCase();
+
+            assertThat(productName, containsString(searchText));
+        }
+    }
+}
